3b0ef7cec3e4fd6d66fc3dacf1827588de6367bd
qht711 <qht711@163.com>
2018-09-11 11:15:15 +0800

    Signed-off-by: qht711 <qht711@163.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello,World");
-        System.out.println("测试一下github提交更改");
+        System.out.println("娴嬭瘯涓�涓媑ithub鎻愪氦");
     }
 }
