f1dc7127c39d512d275ffbd08f6ae89d3dd8ac92
qht711 <qht711@vip.163.com>
2019-05-09 09:02:35 +0800

    learngit

---
+++ src/HelloQHT.java
@@ -0,0 +1,9 @@
+/**
+ * Created by QHT on 2016/10/10.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,World");
+        System.out.println("测试一下github提交");
+    }
+}
